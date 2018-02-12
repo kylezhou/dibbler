fd313a7955c3db6b1182d435bc8b40d536ae466b
Kyle Zhou <kyle.zhou@live.com>
2018-02-13 00:43:02 +1100

    Fixed option IA prefix parsing issue

--- SrvOptions/SrvOptIAPrefix.cpp
+++ SrvOptions/SrvOptIAPrefix.cpp
@@ -21,42 +21,6 @@
 TSrvOptIAPrefix::TSrvOptIAPrefix( char * buf, int bufsize, TMsg* parent)
     :TOptIAPrefix(buf,bufsize, parent)
 {
-    int pos=0;
-    while(pos<bufsize)
-    {
-        uint16_t code = readUint16(buf + pos);
-        pos += sizeof(uint16_t);
-        int length = readUint16(buf + pos);
-        pos += sizeof(uint16_t);
-        if ((code>0)&&(code<=24))
-        {
-            if(allowOptInOpt(parent->getType(),OPTION_IAPREFIX,code))
-            {
-                SPtr<TOpt> opt;
-                opt = SPtr<TOpt>();
-                switch (code)
-                    {
-                    case OPTION_STATUS_CODE:
-                        opt.reset(new TOptStatusCode(buf+pos,length,this->Parent));
-                        break;
-                    default:
-                        Log(Warning) << "Option " << code<< " not supported "
-                                     << " in message (type="<< parent->getType() <<")." << LogEnd;
-                        break;
-                    }
-                if (opt && opt->isValid()) {
-                    SubOptions.append(opt);
-                }
-            } else {
-                Log(Warning) << "Illegal option received, opttype=" << code
-                             << " in field options of IA_PD option" << LogEnd;
-            }
-        } else {
-            Log(Warning) <<"Unknown option in option IAPREFIX(optType="
-                         << code << "). Option ignored." << LogEnd;
-        };
-        pos+=length;
-    }
 }
 
 TSrvOptIAPrefix::TSrvOptIAPrefix(SPtr<TIPv6Addr> prefix, char length, unsigned long pref,
